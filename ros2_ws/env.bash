2aba8b7977cf0af9f085c938f26a938cee78bf59
AutreTechLab <autretechlab@gmail.com>
2020-10-18 16:05:41 +0200

    working Thymio Simulation prototype

---
+++ ros2_ws/env.bash
@@ -0,0 +1,4 @@
+source /opt/ros/foxy/setup.bash
+#export ROS2_WEBOTS_HOME="/usr/local/webots"
+export WEBOTS_HOME="/usr/local/webots"
+source ~/Documents/GitHub/ATL_WEBOTS_ROS2_bridge/ros2_ws/install/local_setup.bash
--- ros2_ws/src/webots_ros2_bridge_py_pkg/webots_ros2_bridge_py_pkg/webots_thymio2_controller_node.py
+++ ros2_ws/src/webots_ros2_bridge_py_pkg/webots_ros2_bridge_py_pkg/webots_thymio2_controller_node.py
@@ -9,11 +9,10 @@ from webots_interfaces.srv import WebotsThymio2ControllerSrv
 from webots_interfaces.srv import WebotsThymio2MotorSrv
 from webots_interfaces.srv import WebotsThymio2LEDSrv
 
-from controller import Robot
 import math
 
 class WebotsThymio2ontrollerNode(WebotsNode): # MODIFY NAME
-    def __init__(self):
+    def __init__(self, args):
         super().__init__("WebotsThymio2ControllerNode")
         self.get_logger().info("Default WEBOTS_ROBOT_NAME env is " + str(os.environ["WEBOTS_ROBOT_NAME"]) )
         self.declare_parameter("robot_device", "Thymio2_V1")
@@ -30,87 +29,84 @@ class WebotsThymio2ontrollerNode(WebotsNode): # MODIFY NAME
 
         self.timer_ = self.create_timer(0.5, self.publish_thymio_status)
 
-        robot = Robot()
-        timestep = int(robot.getBasicTimeStep())
-
         # Initialize Sensors
         ## Proximity
 
-        self.prox_horizontal_0 = robot.getDistanceSensor("prox.horizontal.0")
-        self.prox_horizontal_0.enable(timestep)
-        self.prox_horizontal_1 = robot.getDistanceSensor("prox.horizontal.1")
-        self.prox_horizontal_1.enable(timestep)
-        self.prox_horizontal_2 = robot.getDistanceSensor("prox.horizontal.2")
-        self.prox_horizontal_2.enable(timestep)
-        self.prox_horizontal_3 = robot.getDistanceSensor("prox.horizontal.3")
-        self.prox_horizontal_3.enable(timestep)
-        self.prox_horizontal_4 = robot.getDistanceSensor("prox.horizontal.4")
-        self.prox_horizontal_4.enable(timestep)
-        self.prox_horizontal_5 = robot.getDistanceSensor("prox.horizontal.5")
-        self.prox_horizontal_5.enable(timestep)
-        self.prox_horizontal_6 = robot.getDistanceSensor("prox.horizontal.6")
-        self.prox_horizontal_6.enable(timestep)
+        self.prox_horizontal_0 = self.robot.getDistanceSensor("prox.horizontal.0")
+        self.prox_horizontal_0.enable(self.timestep)
+        self.prox_horizontal_1 = self.robot.getDistanceSensor("prox.horizontal.1")
+        self.prox_horizontal_1.enable(self.timestep)
+        self.prox_horizontal_2 = self.robot.getDistanceSensor("prox.horizontal.2")
+        self.prox_horizontal_2.enable(self.timestep)
+        self.prox_horizontal_3 = self.robot.getDistanceSensor("prox.horizontal.3")
+        self.prox_horizontal_3.enable(self.timestep)
+        self.prox_horizontal_4 = self.robot.getDistanceSensor("prox.horizontal.4")
+        self.prox_horizontal_4.enable(self.timestep)
+        self.prox_horizontal_5 = self.robot.getDistanceSensor("prox.horizontal.5")
+        self.prox_horizontal_5.enable(self.timestep)
+        self.prox_horizontal_6 = self.robot.getDistanceSensor("prox.horizontal.6")
+        self.prox_horizontal_6.enable(self.timestep)
         self.get_logger().info("prox.horizontal 0-6 initiatialised")
-        self.prox_ground_0 = robot.getDistanceSensor("prox.ground.0")
-        self.prox_ground_0.enable(timestep)
-        self.prox_ground_1 = robot.getDistanceSensor("prox.ground.1")
-        self.prox_ground_1.enable(timestep)
+        self.prox_ground_0 = self.robot.getDistanceSensor("prox.ground.0")
+        self.prox_ground_0.enable(self.timestep)
+        self.prox_ground_1 = self.robot.getDistanceSensor("prox.ground.1")
+        self.prox_ground_1.enable(self.timestep)
         self.get_logger().info("prox.ground 0-1 initiatialised" + str(self.prox_horizontal_6))
 
         # Actuators
         ## Motors
-        self.left_motor = robot.getMotor("motor.left")
-        self.right_motor = robot.getMotor("motor.right")
+        self.left_motor = self.robot.getMotor("motor.left")
+        self.right_motor = self.robot.getMotor("motor.right")
         self.left_motor.setPosition(float('inf'))
         self.right_motor.setPosition(float('inf'))
         self.left_motor.setVelocity(4.0)
         self.right_motor.setVelocity(4.0)
-        self.motorMaxSpeed = self.leftMotor.getMaxVelocity()
+        self.motorMaxSpeed = self.left_motor.getMaxVelocity()
         self.get_logger().info("motor.left/right initiatialised")
 
         print("ALT Controller ready, let's do this!")
         # ## LEDs
         # LED_FREQ = 4.0
-        # time = robot.getTime()
+        # time = self.robot.getTime()
         # # rgb_red = float(0.5 * math.sin(math.pi() / float(LED_FREQ) * float(time)) + 0.5);
         # # rgb_green = 0.5 * math.sin(math.pi() / float(LED_FREQ) * float(time) + float(math.pi()/3)) + 0.5;
         # # rgb_blue = 0.5 * math.sin(math.pi() / float(LED_FREQ) * float(time) + float(2*math.pi()/3)) + 0.5;
         #
-        # leds_top = robot.getLED("leds.top")
-        # leds_bottom_left = robot.getLED("leds.bottom.left")
-        # leds_bottom_right = robot.getLED("leds.bottom.right")
+        # leds_top = self.robot.getLED("leds.top")
+        # leds_bottom_left = self.robot.getLED("leds.bottom.left")
+        # leds_bottom_right = self.robot.getLED("leds.bottom.right")
         # leds_buttons_led = [
-        #     robot.getLED("leds.buttons.led0"),
-        #     robot.getLED("leds.buttons.led1"),
-        #     robot.getLED("leds.buttons.led2"),
-        #     robot.getLED("leds.buttons.led3")
+        #     self.robot.getLED("leds.buttons.led0"),
+        #     self.robot.getLED("leds.buttons.led1"),
+        #     self.robot.getLED("leds.buttons.led2"),
+        #     self.robot.getLED("leds.buttons.led3")
         # ]
         # leds_circle_led = [
-        #     robot.getLED("leds.circle.led0"),
-        #     robot.getLED("leds.circle.led1"),
-        #     robot.getLED("leds.circle.led2"),
-        #     robot.getLED("leds.circle.led3"),
-        #     robot.getLED("leds.circle.led4"),
-        #     robot.getLED("leds.circle.led5"),
-        #     robot.getLED("leds.circle.led6")
+        #     self.robot.getLED("leds.circle.led0"),
+        #     self.robot.getLED("leds.circle.led1"),
+        #     self.robot.getLED("leds.circle.led2"),
+        #     self.robot.getLED("leds.circle.led3"),
+        #     self.robot.getLED("leds.circle.led4"),
+        #     self.robot.getLED("leds.circle.led5"),
+        #     self.robot.getLED("leds.circle.led6")
         # ]
         # leds_prox_h_led = [
-        #     robot.getLED("leds.prox.h.led0"),
-        #     robot.getLED("leds.prox.h.led1"),
-        #     robot.getLED("leds.prox.h.led2"),
-        #     robot.getLED("leds.prox.h.led3"),
-        #     robot.getLED("leds.prox.h.led4"),
-        #     robot.getLED("leds.prox.h.led5"),
-        #     robot.getLED("leds.prox.h.led6")
+        #     self.robot.getLED("leds.prox.h.led0"),
+        #     self.robot.getLED("leds.prox.h.led1"),
+        #     self.robot.getLED("leds.prox.h.led2"),
+        #     self.robot.getLED("leds.prox.h.led3"),
+        #     self.robot.getLED("leds.prox.h.led4"),
+        #     self.robot.getLED("leds.prox.h.led5"),
+        #     self.robot.getLED("leds.prox.h.led6")
         # ]
         # leds_prox_v_led = [
-        #     robot.getLED("leds.prox.v.led0"),
-        #     robot.getLED("leds.prox.v.led1")
+        #     self.robot.getLED("leds.prox.v.led0"),
+        #     self.robot.getLED("leds.prox.v.led1")
         # ]
-        # leds_sound = robot.getLED("leds.sound")
-        # leds_rc = robot.getLED("leds.rc")
-        # leds_temperature_red = robot.getLED("leds.temperature.red")
-        # leds_temperature_blue = robot.getLED("leds.temperature.blue")
+        # leds_sound = self.robot.getLED("leds.sound")
+        # leds_rc = self.robot.getLED("leds.rc")
+        # leds_temperature_red = self.robot.getLED("leds.temperature.red")
+        # leds_temperature_blue = self.robot.getLED("leds.temperature.blue")
         #
 
 
